165cb0ba80d59a0898ef6bbff18f68403315d223
edugfilho <edugomfilho@gmail.com>
2014-02-04 22:35:23 -0300

    +1

---
+++ BaggingToolParser/src/database/OutputBagging.java
@@ -0,0 +1,16 @@
+package database;
+
+public abstract class OutputBagging {
+	String baggingQuery;
+
+	public String getBaggingQuery() {
+		return baggingQuery;
+	}
+
+	public void setBaggingQuery(String baggingQuery) {
+		this.baggingQuery = baggingQuery;
+	}
+	
+	
+
+}
